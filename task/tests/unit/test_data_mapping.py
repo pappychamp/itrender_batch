99d1d4527e4727496286a76ffa8d03899aa06dfc
pappychamp <ka3.no3pappy@gmail.com>
2024-09-14 23:29:50 +0900

    add: data_mapping関数のthinkit,techplusのテストを追加

--- task/tests/unit/test_data_mapping.py
+++ task/tests/unit/test_data_mapping.py
@@ -5,6 +5,8 @@ import pytest
 from app.services.data_mapping import (
     data_mapping,
     qiita_data_mapping,
+    techplus_data_mapping,
+    thinkit_data_mapping,
     yahoo_data_mapping,
     youtube_data_mapping,
     zenn_data_mapping,
@@ -100,6 +102,54 @@ async def test_data_mapping_yahoo():
         assert "url" in data
         assert "published_at" in data
         assert isinstance(data["published_at"], datetime)
+@pytest.mark.asyncio
+async def test_data_mapping_thinkit():
+    # テスト用の入力データ
+    site_id = uuid4()
+    article_data_list = [
+        {"title": "Test Article 1", "url": "http://test1.com"},
+        {"title": "Test Article 2", "url": "http://test2.com"},
+    ]
+
+    # data_mapping_funcにthinkit_data_mappingを使用
+    mapped_data = await data_mapping(site_id, article_data_list, thinkit_data_mapping)
+
+    # データをチェック
+    assert len(mapped_data) == 2
+    for original, mapped in zip(article_data_list, mapped_data):
+        assert "site_id" in mapped
+        assert "title" in mapped
+        assert "url" in mapped
+        assert "ranking" in mapped
+
+        # 値を検証
+        assert mapped["title"] == original["title"]
+        assert mapped["url"] == f"https://thinkit.co.jp{original["url"]}"
+
+
+@pytest.mark.asyncio
+async def test_data_mapping_techplus():
+    # テスト用の入力データ
+    site_id = uuid4()
+    article_data_list = [
+        {"title": "Test Article 1", "url": "/articles/1"},
+        {"title": "Test Article 2", "url": "/articles/2"},
+    ]
+
+    # data_mapping_funcにtechplus_data_mappingを使用
+    mapped_data = await data_mapping(site_id, article_data_list, techplus_data_mapping)
+
+    # データをチェック
+    assert len(mapped_data) == 2
+    for original, mapped in zip(article_data_list, mapped_data):
+        assert "site_id" in mapped
+        assert "title" in mapped
+        assert "url" in mapped
+        assert "ranking" in mapped
+
+        # 値を検証
+        assert mapped["title"] == original["title"]
+        assert mapped["url"] == f"https://news.mynavi.jp{original["url"]}"
 
 
 @pytest.mark.asyncio
