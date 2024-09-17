ea4b75a6cf0d37fc04a9d73927b8f69b68ce8013
pappychamp <ka3.no3pappy@gmail.com>
2024-09-17 21:31:44 +0900

    add: image画像を取得する関数のテスト追加

--- task/tests/unit/test_zenn.py
+++ task/tests/unit/test_zenn.py
@@ -2,6 +2,7 @@ import aiohttp
 import pytest
 from aioresponses import aioresponses
 from app.utils.zenn import ZennAPI
+from bs4 import BeautifulSoup
 
 
 @pytest.fixture
@@ -47,3 +48,103 @@ async def test_zenn_fetch_article_exception(zenn_api):
         # テスト
         with pytest.raises(aiohttp.ClientError):
             await zenn_api.fetch_article()
+
+
+@pytest.mark.asyncio
+async def test_zenn_fetch_article_image_success(zenn_api):
+    """
+    fetch_article_imageメソッドの正常テスト
+    """
+    # サンプルHTMLを用意
+    sample_html = """
+        <html>
+            <head>
+                <meta property="og:image" content="test_content">
+            </head>
+        </html>
+        """
+    soup = BeautifulSoup(sample_html, "html.parser")
+    # テスト
+    result = await zenn_api.fetch_article_image(soup)
+    assert result == "test_content"
+
+
+@pytest.mark.asyncio
+async def test_zenn_fetch_article_image_no_a_tag(zenn_api):
+    """
+    fetch_article_imageメソッドのmetaタグがない場合の正常テスト
+    """
+    # サンプルHTMLを用意
+    sample_html = """
+        <html>
+            <head>
+                <metas property="og:image" content="test_content">
+            </head>
+        </html>
+        """
+    soup = BeautifulSoup(sample_html, "html.parser")
+    # テスト
+    result = await zenn_api.fetch_article_image(soup)
+    assert result is None
+
+
+@pytest.mark.asyncio
+async def test_zenn_fetch_article_image_exception(zenn_api):
+    """
+    fetch_article_imageメソッドの例外発生時のテスト
+    """
+    soup = None
+
+    # pytest.raisesを使って例外が発生するかを確認
+    with pytest.raises(Exception):
+        await zenn_api.fetch_article_image(soup)
+
+
+@pytest.mark.asyncio
+async def test_zenn_fetch_article_tag_success(zenn_api):
+    """
+    fetch_article_tagメソッドの正常テスト
+    """
+    # サンプルHTMLを用意
+    sample_html = """
+        <html>
+            <div class="View_topicName__test">
+                test_tag
+            </div>
+        </html>
+        """
+    soup = BeautifulSoup(sample_html, "html.parser")
+    # テスト
+    result = await zenn_api.fetch_article_tag(soup)
+    assert result == ["test_tag"]
+
+
+@pytest.mark.asyncio
+async def test_zenn_fetch_article_tag_no_a_tag(zenn_api):
+    """
+    fetch_article_tagメソッドのmetaタグがない場合の正常テスト
+    """
+    # サンプルHTMLを用意
+    sample_html = """
+        <html>
+            <div class="test_View_topicName__test">
+                test_tag
+            </div>
+        </html>
+        """
+    soup = BeautifulSoup(sample_html, "html.parser")
+    # テスト
+    result = await zenn_api.fetch_article_tag(soup)
+    assert result == []
+
+
+@pytest.mark.asyncio
+async def test_zenn_fetch_article_tag_exception(zenn_api):
+    """
+    fetch_article_tagメソッドの例外発生時のテスト
+    """
+    soup = None
+
+    # pytest.raisesを使って例外が発生するかを確認
+    with pytest.raises(Exception):
+        await zenn_api.fetch_article_tag(soup)
